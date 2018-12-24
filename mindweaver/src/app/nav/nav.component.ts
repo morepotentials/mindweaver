b344ec977b311e46d15003a9557e61998758d2a1
Austin Kellar <ajk@Austins-MacBook-Pro-2.local>
2018-12-24 10:52:49 -0700

    restyled navbar on home screen

--- mindweaver/src/app/nav/nav.component.ts
+++ mindweaver/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Subscription } from 'rxjs';
 import { ChangeDetectorRef } from '@angular/core'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -14,7 +15,7 @@ export class NavComponent implements OnInit, OnDestroy {
   user: any;
   subscription: Subscription;
 
-  constructor(private authService: AuthService, private ref: ChangeDetectorRef) {
+  constructor(private authService: AuthService, private ref: ChangeDetectorRef, public router: Router) {
     this.subscription = this.authService.getUser().subscribe(update => { 
       this.user = update.user; 
       ref.detectChanges();
@@ -30,6 +31,9 @@ export class NavComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    setInterval(() => {
+      console.log(this.router.url);
+    }, 500);
   }
 
   ngOnDestroy() {
